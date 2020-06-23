9d3859e016d5c1c2852d2c793c9edab6592b6cb5
liuxie <zyhd.liuxie@waibao.cntaiping.com>
2020-06-23 09:08:07 +0800

    ArrayList学习

--- src/com/ArrayListDemo/ArrayListDemo.java
+++ src/com/ArrayListDemo/ArrayListDemo.java
@@ -14,13 +14,36 @@ import java.util.ArrayList;
  *          E:泛型
  *          public boolean add(E, e): 添加元素
  *          public void add(int index, E element):在指定的索引处添加一个元素
+ * 获取元素：
+ *      public E get(int index):返回指定索引处的元素
+ * 集合长度：
+ *      public int size(): 返回集合中的元素的个数
+ * 删除元素：
+ *      public boolean remove(Object o): 删除指定元素，返回删除是否成功
+ *      public E remove(int index): 删除指定索引处元素，返回被删除的元素
+ * 修改元素：
+ *      public E set(int index, E element): 修改指定索引处的元素，返回被修改的元素
  * */
 public class ArrayListDemo {
     public static void main(String[] args) {
+//      创建集合对象
         ArrayList<String> array = new ArrayList<String>();
         System.out.println(array);
         array.add("hello");
         array.add("world");
+        array.add("java");
+        System.out.println(array);
+//      获取
+        System.out.println(array.get(0));
+        System.out.println(array.get(1));
+        System.out.println(array.get(2));
+//      长度
+        System.out.println(array.size());
+//      删除
+        System.out.println(array.remove("world"));
+        System.out.println(array.remove(0));
+//      修改
+        System.out.println(array.set(0, "android"));
         System.out.println(array);
     }
 }
---
+++ src/com/ArrayListDemo/ArrayListTest.java
@@ -0,0 +1,44 @@
+package com.ArrayListDemo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListTest {
+    public static void main(String[] args) {
+        String[] strArr = { "张三丰", "宋远桥", "张无忌", "殷梨亭", "张翠山", "灭绝师太" };
+        ArrayList<String> arrayList = new ArrayList<String>();
+
+        for (int i = 0; i < strArr.length; i++) {
+            arrayList.add(strArr[i]);
+        }
+
+        for (int x = 0; x < arrayList.size(); x++) {
+            String s = arrayList.get(x);
+            if (s.startsWith("张")) {
+                System.out.println(s);
+            }
+        }
+
+        System.out.println(arrayList);
+
+        ArrayList<Student> array = new ArrayList<Student>();
+        for (int i = 0; i < 2; i++) {
+            addStu(array);
+        }
+        for (int i = 0; i < array.size(); i++) {
+            Student s = array.get(i);
+            System.out.println(s.getAge() + "----" + s.getName());
+        }
+    }
+
+    public static void addStu(ArrayList<Student> arr) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入姓名：");
+        String name = sc.nextLine();
+        System.out.println("请输入年龄：");
+        String age = sc.nextLine();
+        Student st = new Student();
+        st.setName(name);
+        st.setAge(age);
+        arr.add(st);
+    }
+}
