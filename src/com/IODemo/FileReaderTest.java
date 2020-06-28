0300f9efccc60fdaac12c4d9c242ff6607fd3e43
liuxie <zyhd.liuxie@waibao.cntaiping.com>
2020-06-28 18:13:07 +0800

    读文件--

--- src/com/IODemo/FileReaderTest.java
+++ src/com/IODemo/FileReaderTest.java
@@ -6,6 +6,7 @@ package com.IODemo;
  *      int read(): 一次读取一个字符
  * */
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 public class FileReaderTest {
     public static void main(String[] args) throws IOException {
@@ -16,6 +17,27 @@ public class FileReaderTest {
         while ((ch = fr.read()) != -1) {
             System.out.print((char) ch);
         }
+        System.out.print("\r\n");
+//      读数据
+        FileReader fr1 = new FileReader("fr.txt");
 
+        char[] chs = new char[5];
+//      第一次读取
+//        int len = fr1.read(chs);
+//        System.out.println(len);
+//        System.out.println(new String(chs));
+//      第二次读取
+//        len = fr1.read(chs);
+//        System.out.println(len);
+//        System.out.println(new String(chs));
+//      第三次读取
+//        len = fr1.read(chs);
+//        System.out.println(len);
+//        System.out.println(new String(chs));
+//
+        int len;
+        while ((len = fr1.read(chs)) != -1) {
+            System.out.print(new String(chs, 0, len));
+        }
     }
 }
